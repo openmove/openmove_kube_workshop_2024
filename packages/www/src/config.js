2f11ab6c6c79cbec5117e1ca5923004857a25d56
Riccardo Martinelli <riccardo.martinelli@openmove.com>
2024-09-24 14:08:53 +0200

    update www

--- packages/www/src/config.js
+++ packages/www/src/config.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const config = {
-    port: process.env.PORT | 8080
-}
+    'port': process.env.PORT || 8080,
+    'api': {
+        'baseUrl': process.env.API_BASE_URL || 'http://localhost:3001'
+    }
+};
 
 module.exports = config;
