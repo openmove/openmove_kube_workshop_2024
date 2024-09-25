a9e2001042a250d625b7b4487e93fc8556b68954
yoza <andreaiossa97@gmail.com>
2024-09-25 09:15:54 +0200

    [imager] added try catch to avoid breaking errors

--- packages/imaginer/src/actions/process_image.py
+++ packages/imaginer/src/actions/process_image.py
@@ -9,28 +9,31 @@ def callback_factory(confs, models):
   def callback(ch, method, properties, body):
     confs.log.info('new message received')
 
-    json_body = body.decode()
-    data = json.loads(json_body)
-    storage = models['storage']
-    images = models['images']
+    try:
+      json_body = body.decode()
+      data = json.loads(json_body)
+      storage = models['storage']
+      images = models['images']
 
-    confs.log.debug(f'new image to parse called: {data['filename']}')
-    document = storage.retrieve_image(confs, data['imageId'])
-    image = images.decode_binary(document['data'])
+      confs.log.debug(f'new image to parse called: {data['filename']}')
+      document = storage.retrieve_image(confs, data['imageId'])
+      image = images.decode_binary(document['data'])
 
-    processed_image = random.choice([
-      images.blur,
-      images.rotate,
-      images.invert,
-      images.pixelate
-    ])(image)
+      processed_image = random.choice([
+        images.blur,
+        images.rotate,
+        images.invert,
+        images.pixelate
+      ])(image)
 
-    processed_data = images.encode_binary(processed_image)
-    storage.store_processed_image(confs, processed_data, data)
+      processed_data = images.encode_binary(processed_image)
+      storage.store_processed_image(confs, processed_data, data)
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-    confs.log.debug("Process of image completed")
-
+      ch.basic_ack(delivery_tag=method.delivery_tag)
+      confs.log.debug("Process of image completed")
+    except Exception as e:
+      confs.log.warning(e)
+      ch.basic_ack(delivery_tag=method.delivery_tag)
 
   return callback
 
